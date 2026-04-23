3533751b20ba51ee88eb8e96d9a00e498bc92678
bigbosscreator123 <shawnseo123@gmail.com>
2026-04-22 17:31:36 -0700

    finished and wrote comments

--- wk13_program/wk13_program1.cpp
+++ wk13_program/wk13_program1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+
+// recursive function. until n < 1, call sign
 void sign(int n) {
+    //handles any negative numbers and 0
     if (n > 0) {
         std::cout << "No Parking\n";
         sign(n - 1);
@@ -14,6 +17,7 @@ int main() {
     std::cin >> input;
     std::cout << std::endl;
 
+    // call the recursive function with the input
     sign(input);
 
     return 0;
